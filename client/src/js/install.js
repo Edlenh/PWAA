bf36e1e93591671af83dd28a36f8de0c5ea07a76
Edward <herreraed93@gmail.com>
2023-03-17 14:14:13 -0700

    first commit

---
+++ client/src/js/install.js
@@ -0,0 +1,19 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+    butInstall.addEventListener('click', async () => {
+    event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+  });
+});;
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+  });
+  
